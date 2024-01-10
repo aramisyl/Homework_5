4c5b35a4c64dcad24b3dd9c3801b4f92edc93ea1
Yuliya Laurynovich <y.laurynovich@melsoft-games.com>
2024-01-11 00:37:33 +0100

    added default case and improved outputs for several cases

--- Task_10.py
+++ Task_10.py
@@ -1,8 +1,14 @@
 initial_word = input("Please provide a word in singular.").lower()
 def pluralize(word):
     match word[-1]:
-        case "s" | "x" | "z" | "ch" | "sh":
-            word = word[:-1] + 'es'
+        case "s" | "x" | "z":
+            word = word + 'es'
+            return word
+        case "h":
+            if word [-2] in ('s', 'c'):
+                word = word + 'es'
+            else:
+                word = word + "s"
             return word
         case "y":
             if word[-2] in ["a", "e", "i", "o", "u"]:
@@ -10,5 +16,9 @@ def pluralize(word):
             else:
                 word = word[:-1] + "ies"
             return word
+        case _:
+            return word + "s"
+
+
 
 print(pluralize(initial_word))
